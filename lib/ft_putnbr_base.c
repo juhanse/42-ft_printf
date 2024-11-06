c26bbc2dbdfcc1e7fe7b12fde97bb6c723c43a8d
Julien Hanse <juhanse@shi-r8-p3.s19.be>
2024-11-06 12:07:51 +0100

    fix: norme

--- lib/ft_putnbr_base.c
+++ lib/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 23:10:30 by julienhanse       #+#    #+#             */
-/*   Updated: 2024/11/02 11:50:20 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/11/06 12:06:33 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,14 @@ static void	ft_putchar_base(int nbr, char *base, int *count)
 void	ft_putnbr_base(int nbr, int sign, char *base_set, int *count)
 {
 	unsigned int	size;
-	unsigned int	un;
 
-	un = nbr;
 	size = ft_strlen(base_set);
 	if (sign && nbr < 0)
 	{
-		un = -nbr;
+		nbr *= -1;
 		ft_putchar('-', count);
 	}
-	if (un > size - 1)
-		ft_putnbr_base(un / size, sign, base_set, count);
-	ft_putchar_base((un % size), base_set, count);
+	if (nbr > size - 1)
+		ft_putnbr_base(nbr / size, sign, base_set, count);
+	ft_putchar_base((nbr % size), base_set, count);
 }
