9b77cb5a75643fd19ce7006748930665f3e64af7
Julien Hanse <juhanse@shi-r8-p3.s19.be>
2024-11-06 12:50:08 +0100

    fix: error in ft_putnbr_base.c

--- lib/ft_putnbr_base.c
+++ lib/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 23:10:30 by julienhanse       #+#    #+#             */
-/*   Updated: 2024/11/06 12:06:33 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/11/06 12:48:44 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,17 @@ static void	ft_putchar_base(int nbr, char *base, int *count)
 
 void	ft_putnbr_base(int nbr, int sign, char *base_set, int *count)
 {
+	unsigned int	tmp;
 	unsigned int	size;
 
+	tmp = nbr;
 	size = ft_strlen(base_set);
 	if (sign && nbr < 0)
 	{
-		nbr *= -1;
+		tmp = -nbr;
 		ft_putchar('-', count);
 	}
-	if (nbr > size - 1)
-		ft_putnbr_base(nbr / size, sign, base_set, count);
-	ft_putchar_base((nbr % size), base_set, count);
+	if (tmp > size - 1)
+		ft_putnbr_base(tmp / size, sign, base_set, count);
+	ft_putchar_base((tmp % size), base_set, count);
 }
